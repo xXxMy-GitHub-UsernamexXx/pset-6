2d0949c8a315f187c612d35e728f5d69cdf0f74f
Ethan Charles <bigetc1723@gmail.com>
2020-01-05 20:38:48 -0500

    added function to create items

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
 window.onload = function() {
-        document.getElementById("add button").onclick = newItem;
+        document.getElementById("add item").onsubmit = newItem;
 
 };
 
 let itemList = [];
 
-let itemPrototype = new Object();
-itemPrototype.description = "blank";
-itemPrototype.priority = false;
-itemPrototype.completion = false;
+let itemPrototype = Object.create(Object.prototype);
+itemPrototype["description"] = "blank";
+itemPrototype["priority"] = "low";
+itemPrototype["completion"] = "not completed";
 
 const newItem = function() {
-
+        let addedItem = Object.create(itemPrototype)
+        let itemDescription = document.getElementById("add item").getElementsByTagName("input");
+        newItem["description"] = itemDescription;
+        itemList.push(newItem);
+        let newItemForList = document.createElement("li");
+        let list = document.querySelector("html body ul");
+        list.append(newItemForList);
 
 };
